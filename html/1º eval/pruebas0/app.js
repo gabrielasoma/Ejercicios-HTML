57b4b160972410012baf5c52292202f8a9bb7f54
Gabriela <gabrielasoma19@gmail.com>
2023-03-25 14:26:25 +0100

    Ejercicio html 1º evaluación

---
+++ html/1º eval/pruebas0/app.js
@@ -0,0 +1,19 @@
+document.write("<h1>Hello world</h1>");
+
+//tipos de datos
+"hola"     //string
+'hola'     //string
+
+10000 //number
+-2.3
+
+
+//boolean
+true 
+false 
+
+//array
+['joy','ryan'];
+[1,2];
+[true, false]
+
